c4331043a89c5638ef6411f33ea1d2f0f82b35f7
elpidabantra <elpidabantra@gmail.com>
2017-02-21 17:09:57 +0000

    Update seat_assign_16200254_16200947_16204693.py
    Added functions to get number of cols and number of rows in the airplane

--- seat_assign_16200254_16200947_16204693.py
+++ seat_assign_16200254_16200947_16204693.py
@@ -2,8 +2,8 @@
 # AIRLINE RESERVATION SYSTEM
 # ROHIT SINGH ELPIDA BANTRA LISHA GHOSH
 # MSC BUSINESS ANALYTICS
-# Updated 20th Feb by Rohit Singh
 
+#updated by Elpida
 # *******************************************************Import libraries into python************************************************#
 import numpy as np  # Import numpy
 import pandas as pd  # Import pandas
@@ -29,7 +29,7 @@ def db_connection():
 
 
 connection =db_connection()
-
+#updated by Lisha
 # *******************************************************Retrieve and update database metrics*******************************************************#
 def retrieve_metrics():
     global cursor, refused_pass, sep_pass
@@ -62,7 +62,7 @@ def update_seat_allocation(passenger_name, num_row, num_col):
                       , (passenger_name, (num_row+1), FLIGHT_SEATS[num_col+1]))
     connection.commit()
     return
-
+#updated by Rohit
 # *******************************************************Flight seating layout************************************************#
 # Function to get total number of seats
 def layout_get_total_seats():
@@ -98,3 +98,29 @@ def layout_get_occupied_seats():
     for row in cursor.execute('select row, seat from seating where name != "" '):
         Occ_Seats.append(row)
     return Occ_Seats
+
+#updated by Elpida
+# Function to get number of columns in the airplane
+def layout_get_cols(Avl_Seats, All_seats=layout_get_total_seats()):
+    x = []
+    for l in zip(*All_seats):
+        b = set(l)
+        print(b)
+    max_cols = set(l)
+    col_count = len(max_cols)
+    return col_count
+
+
+col_count = layout_get_cols(Avl_Seats, All_seats=layout_get_total_seats())
+
+
+# Function to get number of rows in the airplane
+def layout_get_rows(Avl_Seats, All_seats):
+    max_rows = np.array([max(Avl_Seats)])
+    row_count_1 = max_rows[:, 0]
+    row_count_2 = np.asscalar(row_count_1)
+    row_count = np.int(row_count_2)
+    return row_count
+
+
+row_count = layout_get_rows(Avl_Seats, All_seats=layout_get_total_seats())
